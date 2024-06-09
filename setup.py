1a4f71879adc9f054902ca32962a6702a7d0e4aa
mspatke <harshadapatke@gmail.com>
2024-06-09 01:14:22 +0100

    update url

--- setup.py
+++ setup.py
@@ -20,10 +20,7 @@ setuptools.setup(
     description="A small python package for ml app",
     long_description=long_description,
     long_description_content="text/markdown",
-    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
-    project_urls={
-        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
-    },
+    url='',
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src")
 )
